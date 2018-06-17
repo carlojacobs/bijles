4d0171ba0a418c3ba53d9f545c34e81f56694579
Carlo Jacobs <carlojacobs91@gmail.com>
2018-06-17 22:50:31 +0200

    Some changes

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -25,7 +25,7 @@ export default class Frontpage extends Component {
           <div className="col-md-9">
             <div className="jumbotron">
               <h1>Hulp nodig voor betere cijfers?</h1>
-              <p className="lead">Zit uw kind in de eerste of tweede klas op het Revius Lyceum Doorn, en kan hij of zij wel wat hulp gebruiken? Wij helpen graag! Onze ervaren revianen zijn gespecializeerd in het leren voor en maken van toetsen.</p>
+              <p className="lead">Zit uw kind in de eerste of tweede klas op het Revius Lyceum Doorn, en kan hij of zij wel wat hulp gebruiken? Wij helpen graag! Als ervaren Revianen zijn wij gespecialiseerd in het leren voor en maken van toetsen.</p>
             </div>
           </div>
           <div className="col-md-3">
@@ -40,11 +40,11 @@ export default class Frontpage extends Component {
         </div>
         <div className="row">
           <div className="col-md-6">
-            <div className="card">
+            <div className="card carlo-card">
               <div className="card-body">
                 <h5 className="card-title">Wie zijn wij?</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Goede vraag!</h6>
-                <p className="card-text" style={{fontSize: 1.2+'rem'}}>Wij zijn een groep leerlingen die in de vierde klas of hoger zitten, en veel ervaring hebben met toetsen, huiswerk en andere schoolzaken. Wij zouden uw kind graag willen helpen om zijn of haar cijfers te verbeteren.</p>
+                <p className="card-text" style={{fontSize: 1.2+'rem'}}>Wij zijn een groep leerlingen die in de vierde klas of hoger zitten, en veel ervaring hebben met toetsen, huiswerk en andere schoolzaken. Graag helpen wij uw kind om zijn of haar cijfers te verbeteren.</p>
                 <button onClick={this.scrollToTutors} className={"btn btn-lg btn-outline-" + this.state.style}>Oke! Wie kan ik inhuren?</button>
               </div>
             </div>
@@ -53,9 +53,102 @@ export default class Frontpage extends Component {
             <img src="https://architectenweb.nl/media/illustrations/2016/05/32d63c4a-e671-42b6-ab95-98c8827bfecb_400.jpg" alt="school" className="rounded img-fluid"/>
           </div>
         </div>
-        <div id="info" className="container mt-5 border rounded pt-4 pr-4 pl-4 pb-4">
-          <h1>Hoe geven wij bijles?</h1>
-          <h5>Wij willen uw kind graag helpen me het beter begrijpen van de schoolstof. Vaak helpt het om opdrachten te maken en fouten goed te analyseren en te begrijpen, zodat deze niet meer gemaakt worden. Als voormalige eerste en tweede klassers weten wij precies waar de valkuilen liggen en hoe deze vermeden kunnen worden.</h5>
+        <div className="row">
+          <div className="col-md-6 mt-5">
+            <div id="info" className="card carlo-card">
+              <div className="card-body">
+                <h1 className="card-title">Hoe geven wij bijles?</h1>
+                <p className="card-text" style={{fontSize: 1.2+'rem'}}>Als voormalige eerste en tweede klassers weten wij precies waar de valkuilen liggen en hoe deze te vermijden. Wij willen uw kind graag helpen met het beter begrijpen van de schoolstof. Vaak helpt het om opdrachten te maken en fouten goed te analyseren en te begrijpen, zodat deze niet herhaald worden.</p>
+                <ol>
+                  <li><h5>Fouten analyseren</h5></li>
+                  <li><h5>Opdrachten maken</h5></li>
+                  <li><h5>Overleg met de docent</h5></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="container mt-5 col-md-6">
+            <div id="accordion">
+              <div class="card">
+                <div class="card-header" id="headingFive">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                      Welke vakken?
+                    </button>
+                  </h5>
+                </div>
+                <div id="collapseFive" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                  <div class="card-body">
+                    <b>Bijles vakken:</b>
+                    <ul>
+                      <li>Wiskunde</li>
+                      <li>Engelse (ook op TTO niveau)</li>
+                      <li>Natuurkunde / NASK</li>
+                      <li>Scheikunde / NASK</li>
+                      <li>Economie</li>
+                      <li>Geschiedenis</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header" id="headingOne">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                      Waar geven wij bijles?
+                    </button>
+                  </h5>
+                </div>
+                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                  <div class="card-body">
+                    Wij komen bij u <b>thuis</b>, of spreken met uw kind af op school in een <b>geschikte studieruimte</b>.
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header" id="headingOne">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                      Hoe neem ik contact op?
+                    </button>
+                  </h5>
+                </div>
+                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                  <div class="card-body">
+                    Als u contact met ons wilt opnemen, kunt u ons <b>bellen of mailen</b>. Onze contactgegevens zijn hieronder te vinden.
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header" id="headingTwo">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      Hoelang duurt de gemiddelde sessie?
+                    </button>
+                  </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                  <div class="card-body">
+                    Uit ervaring weten wij dat een sessie heet meest effectief is als deze rond <b>een uur of anderhalf</b> duurt.
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header" id="headingThree">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                      Kennismaking?
+                    </button>
+                  </h5>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                  <div class="card-body">
+                    <b>Ter kennismaking</b> is het eerste half uur van de eerste afspraak <b>op onze kosten</b>, hierin worden wat punten doorgenomen en bepalen wij samen de aanpak.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
     return(
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded mt-4">
-          <a className="navbar-brand" href="#">Bijles Revius <i class="fas fa-chalkboard-teacher fa-lg"></i></a>
+          <a className="navbar-brand" href="#">Bijles Doorn <i class="fas fa-chalkboard-teacher fa-lg"></i></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
