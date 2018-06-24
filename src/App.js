68d1a0cdb441c37b59de0011bf05bd285c8c6748
Carlo Jacobs <carlojacobs91@gmail.com>
2018-06-24 12:06:49 +0200

    Small route changes

--- src/App.js
+++ src/App.js
@@ -13,33 +13,19 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Route path="/" component={Navbar}/>
+            <Route path="/(home|tutoren|info)/" exact component={Navbar}/>
             <Route exact path="/" render={() => (
               <Redirect to="/home"/>
             )}/>
             <Route path="/home" exact component={Frontpage}/>
             <Route path="/tutoren" exact component={Tutors}/>
             <Route path="/info" exact component={Info}/>
-            <Route path="/" component={Footer}/>
+            <Route path="/(home|info|tutoren)/" exact component={Footer}/>
           </div>
         </Router>
       </div>
     );
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar/>
-//         <div className="container">
-//           <Frontpage/>
-//           <Tutors/>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
