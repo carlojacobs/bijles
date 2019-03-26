2b562902af5c4f10112b6f60a2c9d63eb26061ae
Carlo Jacobs <carlojacobs91@gmail.com>
2019-03-26 18:02:35 +0100

    Readme update

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,6 +6,7 @@ export default class Info extends Component {
             <div className="container mt-5">
                 <h1>Over mij</h1>
                 <p className="lead">Ik ben Carlo en ik zit in VWO 5 op het <b>Revius Lyceum Doorn</b>. Ik heb in de eerste tot en met de derde klas <b>TTO</b> gedaan en doe op dit moment <b>IB</b> (een vervolg op TTO). Ik heb er dus al vier jaar middelbare school opzitten. Dit betekent dat ik alle ins en outs van het schoolleven ken. Ik kan uw kind helpen om betere cijfers te halen en de stof beter te begrijpen. Dit helpt niet alleen met de bevordering naar de volgende klas, maar bereidt ook goed voor op de komende jaren.</p>
+                <img id="profilepic" src="http://77.170.153.83/me.jpg"></img>
             </div>
         );
     }
